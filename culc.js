0fa8bd01472fa083e2d6d22e84533eaaf3c2504d
artem-reb <156573280+artem-reb@users.noreply.github.com>
2025-03-09 13:19:25 +0300

    Добавил арифметические действия

---
+++ culc.js
@@ -0,0 +1,63 @@
+let number = '';
+function save_digit(pressed_digit) {
+    let calculator_output = document.querySelector(".calculator_output");
+    number = number * 10 + pressed_digit;
+    calculator_output.innerText = number;
+}
+
+function clear_output() {
+    let calculator_output = document.querySelector(".calculator_output");
+    calculator_output.innerText = 0;
+    number = 0;
+}
+
+function delete_last_digit() {
+    let calculator_output = document.querySelector(".calculator_output");
+    let user_input = calculator_output.innerText
+    user_input = Math.floor(user_input / 10);
+    calculator_output.innerText = user_input;
+    number = user_input;
+}
+
+let operation = '';
+let first_number = 0;
+
+function plus() {
+    first_number = +number;
+    clear_output();
+    operation = '+';
+}
+
+function minus() {
+    first_number = +number;
+    clear_output();
+    operation = '-';
+}
+
+function multiply() {
+    first_number = +number;
+    clear_output();
+    operation = '*';
+}
+
+function divide() {
+    first_number = +number;
+    clear_output();
+    operation = '/';
+}
+
+function equal() {
+    let calculator_output = document.querySelector(".calculator_output");
+    let second_number = +calculator_output.innerText  
+    
+    let result = 0;
+    if (operation == '+') {
+        result = first_number + second_number;
+    }
+
+    else if (operation == '-') {
+        result = first_number - second_number;
+    }
+
+    calculator_output.innerText = result;
+}
